0fe26aaad161d2d11491843dd3ce955993f48cc9
Zebulun Arendsee <arendsee@iastate.edu>
2014-05-15 17:45:11 -0500

    Added stubby README and version

---
+++ README.md
@@ -0,0 +1,5 @@
+# brownian-tails
+
+This is a random story builder.
+
+Currently it is a stub ...
--- brownian-tails.py
+++ brownian-tails.py
@@ -3,6 +3,8 @@
 import os
 import generators.character as characters
 
+__version__ = 0.0.0
+
 if __name__ == '__main__':
     wk_path = os.path.split(os.path.abspath(__file__))[0]
     data_path = os.path.join(wk_path, 'data')
